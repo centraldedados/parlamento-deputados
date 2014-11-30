9e881fff4747f5f754bd6a9dab3c9dca4699da8b
Ricardo Lafuente <r@manufacturaindependente.org>
2014-11-30 22:47:38 +0000

    Melhorar o scraper de fotos

--- pic_scrapper.py
+++ pic_scrapper.py
@@ -1,44 +1,37 @@
 #!/usr/bin/python
 """
-
-- extrai imagens de cada deputado
-- ver: depscrap.py
-
+Extrai imagens de cada deputado
 """
-import sys, os
+import os
 from urllib import urlretrieve
 import json
+from zenlog import log
 
-mp_file = '/home/rlafuente/proj/thd/datasets/deputados.json'
-pic_url_formatter='http://app.parlamento.pt/webutils/getimage.aspx?id=%s&type=deputado'
-dest='imgs/'
+mp_file = 'deputados.json'
+pic_url_formatter = 'http://app.parlamento.pt/webutils/getimage.aspx?id=%s&type=deputado'
+dest = 'imgs/'
+
 
 def main():
     if not os.path.exists(dest):
         os.mkdir(dest)
-        print "Directory 'imgs/' created."
+        log.info("Directory 'imgs/' created.")
 
-    # csv_MP=reader(open('MP.csv'),delimiter='|', quotechar='"')
     mp_json = json.loads(open(mp_file, 'r').read())
     for mp_id in mp_json:
-        # only for new ones, remove later
-        if int(mp_id) > 4200:
+        url = pic_url_formatter % mp_id
+        filename = '%s.jpg' % os.path.join(dest, mp_id)
+        if os.path.exists(filename):
+            log.debug("File for id %s already exists, skipping." % mp_id)
             continue
-        print 'retrieving picture with id: %s' % mp_id
+        log.info('Retrieving picture with id: %s' % mp_id)
         try:
-            urlretrieve(pic_url_formatter % mp_id, '%s%s.jpg' % (dest,mp_id))
+            urlretrieve(url, filename)
         except IOError:
-            print '- Socket error! :('
-    
-    print "it's almost done"
-    print 'do find ./imgs/ -size -722c -exec rm {} \;'
-    print 'to clean up things'
+            log.error('Socket error! :(')
+
+    log.info('Done. Now do find ./imgs/ -size -722c -exec rm {} \;')
+    log.info('to clean up things.')
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
