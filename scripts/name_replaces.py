884d2e638d602e5c8b0406eb6b333a31d5ddc98b
Ricardo Lafuente <r@manufacturaindependente.org>
2016-12-03 19:09:18 +0000

    Mais shortnames rectificados

--- scripts/name_replaces.py
+++ scripts/name_replaces.py
@@ -1,34 +1,33 @@
-#-*- coding: utf-8
-
-# Pessoas fora da BD:
-# Sérgio Monteiro
-# Maria Leonor Parreira
-# Lurdes Ribeiro
-# Pedro Silva Marques
-# Juvenal Silva Peneda
-
-
+#!/usr/bin/env python
+# -*- coding: utf-8
 
 SHORTNAME_REPLACES = {
-        u'António Amaro': u'António Leitão Amaro',
-        u'Maria Gabriela Canavilhas': u'Gabriela Canavilhas',
-        u'José Matos Rosa': u'José de Matos Rosa',
-        u'José Matos Correia': u'José de Matos Correia',
-        u'Maria da Conceição Pereira': u'Maria Conceição Pereira',
-        u'Maria Helena André': u'Helena André',
-        u'Duarte Marques': u'Duarte Filipe Marques',
-        u'Maria Francisca Almeida': u'Francisca Almeida',
-        u'Francisco Assis': u'Francisco de Assis',
-        u'Paulo Baptista Santos': u'Paulo Batista Santos',
-        u'João Antunes': u'João Figueiredo',
-        u'Carlos Gonçalves': u'Carlos Alberto Gonçalves',
-        u'Ana Catarina Mendes': u'Ana Catarina Mendonça',
-        u'António Peixoto': u'Carlos Peixoto',
-        u'Manuel Isaac': u'Fialho Isaac',
-        u'Carlos Amorim': u'Carlos Abreu Amorim',
-        u'João Mota Amaral': u'Mota Amaral',
-        u'Isabel Alves Moreira': u'Isabel Moreira',
-        u'Helder Amaral': u'Hélder Amaral',
-        u'Heitor de Sousa': u'Heitor Sousa',
-        }
-
+    u'António Amaro': u'António Leitão Amaro',
+    u'Maria Gabriela Canavilhas': u'Gabriela Canavilhas',
+    u'José Matos Rosa': u'José de Matos Rosa',
+    u'José Matos Correia': u'José de Matos Correia',
+    u'Maria da Conceição Pereira': u'Maria Conceição Pereira',
+    u'Maria Helena André': u'Helena André',
+    u'Duarte Marques': u'Duarte Filipe Marques',
+    u'Maria Francisca Almeida': u'Francisca Almeida',
+    u'Francisco Assis': u'Francisco de Assis',
+    u'Paulo Baptista Santos': u'Paulo Batista Santos',
+    u'João Antunes': u'João Figueiredo',
+    u'Carlos Gonçalves': u'Carlos Alberto Gonçalves',
+    u'Ana Catarina Mendes': u'Ana Catarina Mendonça',
+    u'António Peixoto': u'Carlos Peixoto',
+    u'Manuel Isaac': u'Fialho Isaac',
+    u'Carlos Amorim': u'Carlos Abreu Amorim',
+    u'João Mota Amaral': u'Mota Amaral',
+    u'Isabel Alves Moreira': u'Isabel Moreira',
+    u'Helder Amaral': u'Hélder Amaral',
+    u'Heitor de Sousa': u'Heitor Sousa',
+    u'Pedro do ó Ramos': u'Pedro do Ó Ramos',
+    u'Isabel Moreira': u'Isabel Alves Moreira',
+    u'Carlos Silva': u'Carlos Santos Silva',
+    u'Álvaro Castelo Branco': u'Álvaro Castello-Branco',
+    u"Filipe Lobo D' Ávila": u"Filipe Lobo d'Ávila",
+    u"Regina Bastos": u"Regina Ramos Bastos",
+    u"Maria Antónia de Almeida Santos": u"Maria Antónia Almeida Santos",
+    u"Maria das Mercês Borges": u"Maria das Mercês Soares",
+}
