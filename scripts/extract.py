2cbc9509733803590c09b494bf7b8fa218322eb9
Ricardo Lafuente <r@manufacturaindependente.org>
2018-06-12 23:02:42 +0100

    Correções ao scraper, juntar makefile

--- scripts/extract.py
+++ scripts/extract.py
@@ -3,32 +3,35 @@
 Extrai a info dos deputados dos JSON vindos do site do Parlamento.
 Recorre aos datasets Registo Biográfico e Info Base
 '''
-import sys
 import os
 import json
 import csv
 from collections import OrderedDict
 import utils
-from pprint import pprint
 
-data_dir = "/home/rlafuente/datasets-central/parlamento/"
-
-leg = sys.argv[1]
-if not leg.startswith("1"):
-    leg = "0" + leg
-
-biofile = os.path.join(data_dir, "data/registo-biografico-{}.json".format(leg))
-infofile = os.path.join(data_dir, "data/info-base-{}.json".format(leg))
-
-biodata = json.load(open(biofile, 'r'))
-infodata = json.load(open(infofile, 'r'))
+data_dir = "../../parlamento/"
 
 # Bios
-biolist = biodata["RegistoBiografico"]["RegistoBiograficoList"]["pt_ar_wsgode_objectos_DadosRegistoBiograficoWeb"]
-# Info sobre ati
-infolist = infodata["Legislatura"]["Deputados"]["pt_ar_wsgode_objectos_DadosDeputadoSearch"]
+biolist = []
+# Info sobre atividade
+infolist = []
 # Registos de interesses
-rgilist = biodata["RegistoBiografico"]["RegistoInteressesV2List"]
+rgilist = []
+
+for leg in range(2, 14):
+    # legislaturas 2 a 13
+    if not str(leg).startswith("1"):
+        leg = "0" + str(leg)
+
+    biofile = os.path.join(data_dir, "data/registo-biografico-{}.json".format(leg))
+    infofile = os.path.join(data_dir, "data/info-base-{}.json".format(leg))
+
+    biodata = json.load(open(biofile, 'r'))
+    infodata = json.load(open(infofile, 'r'))
+
+    biolist.extend(biodata["RegistoBiografico"]["RegistoBiograficoList"]["pt_ar_wsgode_objectos_DadosRegistoBiograficoWeb"])
+    infolist.extend(infodata["Legislatura"]["Deputados"]["pt_ar_wsgode_objectos_DadosDeputadoSearch"])
+    # rgilist.extend(biodata["RegistoBiografico"]["RegistoInteressesV2List"])
 
 mps = []
 for bio in biolist:
@@ -71,6 +74,12 @@ for bio in biolist:
     mp['url_hemiciclo'] = 'http://hemiciclo.pt/{}/{}'.format(mp['party'].lower().replace('-pp', ''), mp['id'])
     mp['image_url'] = 'http://app.parlamento.pt/webutils/getimage.aspx?id={}&type=deputado'.format(mp['id'])
 
+    # já está gravado?
+    results = list(filter(lambda other_mp: other_mp['id'] == mp['id'], mps))
+    if bool(results):
+        # remover o resultado anterior e substituir pelo mais recente
+        other_mp = results[0]
+        mps.remove(other_mp)
     mps.append(mp)
 
 for info in infolist:
@@ -79,6 +88,7 @@ for info in infolist:
     mp = list(filter(lambda mp: mp['id'] == id, mps))
     if len(mp) != 1:
         print("há problema no filtro")
+        print(mp)
         print(len(mp))
     mp = mp[0]
     # situação
@@ -93,16 +103,15 @@ for info in infolist:
                 mp['active'] = True
                 break
 
+# ordenar por ordem alfabética
+mps = sorted(mps, key=lambda k: k['slug'])
+
 # test suite da treta
 active_mps = [mp for mp in mps if mp.get("active")]
-if leg == "13":
-    assert len(active_mps) == 230
-else:
-    assert len(active_mps) == 0
+assert len(active_mps) == 230
 
 
 with open('deputados.csv', 'w') as f:
     writer = csv.DictWriter(f, mps[0].keys())
     writer.writeheader()
     writer.writerows(mps)
-
